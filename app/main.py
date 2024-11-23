376a87d4a3755bee4ec6e8ab6066f817aed5a78f
Auxburger <darius.augsburger@redpoint.teseon.com>
2024-11-23 16:35:59 +0100

    add cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routers.avatar import router as avatar_router
 from app.api.routers.item import router as item_router
@@ -6,6 +7,22 @@ from app.api.routers.quest import router as quest_router
 from app.api.routers.user import router as user_router
 
 app = FastAPI()
+
+# Specify origins allowed to make requests
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://little-big-hero.vulpery.com",
+]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow only specified origins
+    allow_credentials=True,  # Allow cookies to be included in requests
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(user_router)
 app.include_router(quest_router)
